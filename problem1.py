b870eb5c470af19d06e5f6d603b3005480517537
codewith-v <vtiwari99532@gmail.com>
2026-04-11 23:10:48 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,8 @@
 #strings
+#To check if given string is palindrome or not
+#A palindrome reads same backwards
+st=input("Enter the String: ")
+if st[::-1]==st:
+  print('Yes it is Palindrome')
+else:
+  print('Not a Palindrome')
