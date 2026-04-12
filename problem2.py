2bbaefc8fcb2d9a2fb6af68cf3243aed4a11d389
codewith-v <vtiwari99532@gmail.com>
2026-04-12 11:16:43 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,7 @@
 #2 To check if s1 is reverse of s2 
+s1=input('Enter string 1:')
+s2=input('Enter string 2:')
+if s1==s2[::-1]:
+  print('s1 is reverse of s2')
+else:
+  print('s1 is not reverse of s2')
